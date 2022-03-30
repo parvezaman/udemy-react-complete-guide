327307c115ee0176c16ec7fd3371de53d29af044
parvezaman <emailtoamanullah@gmail.com>
2022-03-30 13:04:20 +0600

    adding jsx logic to the components 3/15

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -7,9 +7,20 @@ const ExpenseItem = (props) => {
   // const expensePrice = 215;
 
   // console.log(expenseDate);
+  console.log(props.date.toLocaleString("en-US", { month: "long" }));
+
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  // const year = props.date.toLocaleString("en-US", { year: "numeric" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  // const day = props.date.toLocaleString("en-US", { day: "numeric" });
   return (
     <div className="expense-item">
-      <div className="expense-item__date">{props.date.toDateString()}</div>
+      <div className="expense-item__date">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <div className="expense-item__title">
           <h2>{props.title}</h2>
