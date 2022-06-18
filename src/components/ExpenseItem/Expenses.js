1cf430bb6a9bba74941b9e2b5f50c5e833b3d0b9
parvezaman <emailtoamanullah@gmail.com>
2022-06-18 12:39:09 +0600

    5/5 outputing conditional contents

--- src/components/ExpenseItem/Expenses.js
+++ src/components/ExpenseItem/Expenses.js
@@ -6,27 +6,84 @@ import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
   console.log(props.items);
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p className="no-content-flag">No items to show !!!</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-
-      {props.items.map((expense) => (
+      {/* no filter grab all data */}
+      {/* {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
-      ))}
+      ))} */}
+
+      {/* after applying filter */}
+      {/* {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))} */}
+
+      {/* Now lets say we want to show a message if we dont have any data for the filtered year */}
+      {/* {filteredExpenses.length === 0 ? (
+        <p className="no-content-flag">No items to show !!!</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )} */}
+
+      {/* the same thing we can do using && operator (line: 46-57)*/}
+      {/* {filteredExpenses.length === 0 && (
+        <p className="no-content-flag">No items to show !!!</p>
+      )}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+ */}
+      {/* the same thing we can do using && operator (line: 46-57) and pretty neat way by declaring variable before return*/}
+      {expensesContent}
     </Card>
   );
 };
