bb047b6770fe5e000cb11e067b4e53df0effad91
parvezaman <emailtoamanullah@gmail.com>
2022-06-18 09:43:53 +0600

    5/2 rendering lists of data

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
+      id: "e1",
       title: "Car Insurance",
       amount: "3541",
       date: new Date(2022, 0, 6),
     },
     {
+      id: "e2",
       title: "Toilet paper",
       amount: "465",
       date: new Date(2022, 1, 16),
     },
     {
+      id: "e3",
       title: "House wash",
       amount: "53",
       date: new Date(2022, 2, 6),
     },
     {
+      id: "e4",
       title: "Baking powder",
       amount: "54663",
       date: new Date(2022, 2, 15),
